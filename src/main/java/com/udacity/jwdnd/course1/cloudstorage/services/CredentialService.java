16cf02695e601bfc95567568ecbce2277c009754
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2021-11-03 15:36:42 +0100

    feat: Create CredentialService class

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialService.java
@@ -0,0 +1,33 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mappers.CredentialMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.List;
+
+@Service
+public class CredentialService {
+
+    private CredentialMapper credentialMapper;
+    private EncryptionService encryptionService;
+
+    public CredentialService(CredentialMapper credentialMapper, EncryptionService encryptionService) {
+        this.credentialMapper = credentialMapper;
+        this.encryptionService = encryptionService;
+    }
+
+    public int createCredential(Credential credential) {
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[16];
+        String encodedKey = Base64.getEncoder().encodeToString(key);
+        String encryptedPassword = encryptionService.encryptValue(credential.getPassword(), encodedKey);
+        return credentialMapper.insertCredential(new Credential(null, credential.getUrl(), credential.getUsername(), encryptedPassword, encodedKey, credential.getUserid()));
+    }
+
+    public List<Credential> getUserCredentials(Integer userid) {
+        return credentialMapper.getUserCredentials(userid);
+    }
+}
